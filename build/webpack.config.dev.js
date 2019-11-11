9c744fa5d974877777939d50de15c8c9b28c60da
sebastiancoteanu97 <sebastian.coteanu@arnia.ro>
2019-11-12 01:12:34 +0200

    Tutorial config

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new CopyWebpackPlugin([{
+      from: resolve('static/img'),
+      to: resolve('dist/static/img'),
+      toType: 'dir'
+    }]),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
